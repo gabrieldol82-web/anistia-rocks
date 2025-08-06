fb1efb60576f6907f440f0412eca5ded91999d4d
Gabriel Dantas <gabrieldol82@gmail.com>
2025-08-05 22:12:13 -0300

    add navbar

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,25 @@
+import InstagramIcon  from '@mui/icons-material/Instagram';
+import Tooltip from '@mui/material/Tooltip';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+
+export default function Navbar() {
+    return(
+        <nav className="flex items-center justify-between p-8 bg-transparent text-black font-sans font-nunito text-3xl font-semibold">
+            <ul className="flex space-x-14">
+                <li><a href="/" className="md:hover:border-b md:hover:border-black-800 md:hover:border-solid">Home</a></li>
+                <li><a href="/members" className="md:hover:border-b md:hover:border-black-400 md:hover:border-solid">Membros</a></li>
+                <li><a href="/dates" className="md:hover:border-b md:hover:border-black-400 md:hover:border-solid">Datas</a></li>
+            </ul>
+            <div className="flex items-center space-x-4">
+                <a href="https://www.instagram.com/anistiarocks/" target="_blank" rel="noopener noreferrer">
+                    <InstagramIcon className="md:hover:bg-blue-50 rounded-4xl" width={100} height={100} fontSize="large" />
+                </a>
+                <Tooltip title="Chama no WhatsApp!" arrow>
+                    <a href="https://wa.me/11995474478" target="_blank" rel="noopener noreferrer">
+                        <WhatsAppIcon className="md:hover:bg-green-50 rounded-4xl"  width={100} height={100} fontSize="large" />
+                    </a>
+                </Tooltip>
+            </div>
+        </nav>
+    )
+}

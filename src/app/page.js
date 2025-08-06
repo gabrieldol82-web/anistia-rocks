ca5c238e498f08a4de41b8c9c29e92d0f24b8cc2
Gabriel Dantas <gabrieldol82@gmail.com>
2025-08-06 01:16:48 -0300

    improve structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,7 @@
 import Image from "next/image";
-import anistiaLogo from "./assets/anistiaLogoTransparente.png";
 
 export default function Home() {
   return (
-    <Image src={anistiaLogo} alt="Anistia Logo" width={200} height={100} />
+    <Image src="/assets/anistiaLogoTransparente.png" alt="Anistia Logo" width={200} height={100} />
   );
 }

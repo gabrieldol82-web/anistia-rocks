d665d3aaef5c5331b61fc323b5eab11b6beaa336
Gabriel Dantas <gabrieldol82@gmail.com>
2025-08-11 17:08:16 -0300

    add member detail page and refactor members list

---
+++ src/app/members/[id]/page.js
@@ -0,0 +1,60 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useParams } from "next/navigation";
+import Loading from "../../components/Loading";
+
+export default function Member() {
+  const { id } = useParams();
+  const [member, setMember] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const idName = {
+    1: "gabriel",
+    2: "rafa",
+    3: "gustavo",
+    4: "ana",
+  };
+
+  useEffect(() => {
+    async function fetchMember() {
+      let isMounted = true;
+      try {
+        const name = idName[id];
+        if (!name) {
+          if (isMounted) {
+            setError("Membro não encontrado");
+            setLoading(false);
+          }
+          return;
+        }
+
+        const response = await fetch(`http://localhost:3001/${name}`);
+        if (!response.ok) throw new Error("Erro ao buscar membro.");
+        const data = await response.json();
+
+        if (isMounted) {
+          setMember(data);
+          console.log("Dados do membro:", data);
+        }
+      } catch (err) {
+        if (isMounted) {
+          setError(err.message);
+          setLoading(false);
+        }
+      } finally {
+        if (isMounted) {
+          setLoading(false);
+        }
+      }
+    }
+
+    fetchMember();
+  }, [id]);
+
+  if (loading) return <Loading />;
+  if (error) return <div className="text-red-500">{error}</div>;
+
+  return <>{member.name}</>;
+}
--- src/app/members/page.js
+++ src/app/members/page.js
@@ -1,49 +1,44 @@
+"use client";
+import { useEffect, useState } from "react";
 import Link from "next/link";
+import Loading from "../components/Loading";
 
 export default function Members() {
+  const [members, setMembers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    const members = [
-        {
-            "name": "Gabriel Dantas",
-            "role": "Guitarra",
-            "image": "/assets/capas/gabriel.jpg",
-            "id": 1
-        },
-        {
-            "name": "Rafa Melo",
-            "role": "Vocal",
-            "image": "/assets/capas/rafa.jpg",
-            "id": 2
-        },
-        {
-            "name": "Gutz Pedroza",
-            "role": "Bateria",
-            "image": "/assets/capas/gustavo.jpg",
-            "id": 3
-        },
-        {
-            "name": "Ana Calegari",
-            "role": "Baixo",
-            "image": "/assets/capas/ana.jpg",
-            "id": 4
-        }
-    ];
+  async function fetchMembers() {
+    const response = await fetch("http://localhost:3001/members");
+    const members = await response.json();
+    setMembers(members);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchMembers();
+  }, []);
+
+  if (loading) return <Loading />;
 
   return (
-   <div className="flex flex-wrap justify-center">
-        {members.map(member => {
-            return (
-                <div key={member.id} className="sm:w-full md:w-1/2 p-4 flex flex-col items-center">
-                    <img src={member.image} alt={member.name} className="w-auto h-96 rounded-2xl object-cover mb-4" />
-                    <h2 className="text-xl font-semibold text-black">{member.name}</h2>
-                    <p className="text-gray-600">{member.role}</p>
-                    <Link href={`../members/${member.id}`} >
-                        Ver mais sobre {member.name}
-                    </Link>
-                   
-                </div>
-            );
-        })}
-   </div>
+    <div className="grid grid-cols-2 gap-3">
+      {members.map((member) => {
+        return (
+          <div key={member.id} className="sm:w-full md:w-1/2 p-4 flex flex-col items-center mx-auto">
+            <img
+              src={member.image}
+              alt={member.name}
+              className="w-auto h-96 rounded-2xl object-cover mb-4"
+            />
+            <h2 className="text-xl font-semibold text-black"></h2>
+            <p className="text-gray-600">{member.role}</p>
+            <Link href={`/members/${member.id}`}>
+              Ver mais sobre {member.name}
+            </Link>
+          </div>
+        );
+      })}
+    </div>
   );
 }

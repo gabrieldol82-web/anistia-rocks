73a87e13253e1b267e7be3c976cdad2fd0731b02
Gabriel Dantas <gabrieldol82@gmail.com>
2025-08-22 00:18:37 -0300

    feat: update create-table script to add is_completed column to shows table; remove table creation logic

--- backend/create-table.js
+++ backend/create-table.js
@@ -1,13 +1,8 @@
 import { sql } from "./db.js";
 
 sql`
-    CREATE TABLE shows (
-        id TEXT PRIMARY KEY,
-        title TEXT,
-        location TEXT,
-        description TEXT,
-        show_date TIMESTAMP
-    )
+    ALTER TABLE shows 
+    ADD COLUMN is_completed BOOLEAN
 `.then(() => {
-    console.log('criou a tabela shows')
-})
+    console.log('Coluna is_completed adicionada à tabela shows');
+});

2047c934933a21f3d3c816ec55aa5029547f3640
Gabriel Dantas <gabrieldol82@gmail.com>
2025-08-28 20:08:02 -0300

    fix: adjust album section layout by removing unnecessary responsive grid classes

--- src/app/members/[id]/page.js
+++ src/app/members/[id]/page.js
@@ -94,7 +94,7 @@ export default function Member() {
 
       <section>
         <h2 className="text-2xl font-semibold mb-4">Álbuns Favoritos</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div className="grid grid-cols-1 gap-6">
           {member.albums.map((album, index) => {
             return <SpotifyAlbums albumId={album} key={index} />;
           })}
